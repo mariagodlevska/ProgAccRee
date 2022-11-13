3811f973a7579eb277f8e3576acd16a97aaf8b22
mariagodlevska <113462290+mariagodlevska@users.noreply.github.com>
2022-11-13 19:01:37 +0200

    Update HW4.py

--- HW4.py
+++ HW4.py
@@ -5,30 +5,33 @@
 # ( не використовувати if )
 
 flat = int(input('flat= '))
+n_floor = 9
+n_onfloor = 4
+n_block = 4
 
-pidizd=(flat-1)//36+1
-poverh=((flat-1)%36)//4+1
-na_poversi=(flat+4)%4
+pidizd=(flat-1)//n_floor*n_onfloor+1
+poverh=((flat-1)%(n_floor*n_onfloor))//n_onfloor+1
+na_poversi=(flat+n_onfloor)%n_onfloor
 
-(flat>9*4*4 or flat<0) and print ('wrong number') or flat<=9*4*4 \
-and print (f'pidizd is {pidizd}', f'poverh is {poverh}', f'na_poversi is {na_poversi}')
+(flat>(n_floor*n_onfloor*n_block) or flat<0) and print ('wrong number') or flat<=(n_floor*n_onfloor*n_block) \
+and print (f'pidizd is {pidizd}', f'poverh is {poverh}', f'na poversi is {na_poversi}', sep='\n')
 
 #2. Написати програму, яка буде повертати для заданого року кількість днів. Рік є високосним, якщо він кратний 4,
 # але не кратний 100, а також якщо він кратний 400
 
 year = int(input('year= '))
 
-(year%4==0 and year%100!=0 or year%400==0) and print (year*365+1) or year%4!=0 and print (year*365)
+(year%4 and (not year%100) or year%400) and print (year*365+1) or (not year%4) and print (year*365)
 
-#3. Трикутник існує лише тоді, коли сума будь-яких двох сторін більше третьої. Дано: A, B, C - сторони трикутника.
-# Напишіть програму, яка вказує чи існує такий трикутник.
+# #3. Трикутник існує лише тоді, коли сума будь-яких двох сторін більше третьої. Дано: A, B, C - сторони трикутника.
+# # Напишіть програму, яка вказує чи існує такий трикутник.
 
 a=input('A= ')
 b=input('B= ')
 c=input('C= ')
 
 if (a+b)>c and (c+b)>a and (a+c)>b:
-    print('This is such a triangle.')
+    print('There is such a triangle.')
 
 else:
-    print('This is no such triangle.')
+    print('There is no such triangle.')
